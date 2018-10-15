275bc9e6a1d8fcc350da6a6286e1631a87f72c1a
Johnathan Tebeau <johnathan.tebeau@bhnetwork.com>
2018-10-15 10:07:00 -0500

    Updates to remove date - not needed

--- realpage-app/src/app/app.component.ts
+++ realpage-app/src/app/app.component.ts
@@ -8,5 +8,4 @@ import { Component } from '@angular/core';
 export class AppComponent {
   developer = 'Johnathan M. Tebeau';
   profile = 'https://media.licdn.com/dms/image/C5603AQHV94BM9VmKgw/profile-displayphoto-shrink_100_100/0?e=1545264000&v=beta&t=39aEWbAZ9VXtDeSnVeusru4aIOb5hL2KEoScYAPK6cs';
-  $scope.dateVariable = new Date();
 }
