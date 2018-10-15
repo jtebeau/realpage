5332102c8a81d3d901eb14f674fd212d14f78762
Johnathan Tebeau <johnathan.tebeau@bhnetwork.com>
2018-10-15 10:18:01 -0500

    no message

--- realpage-app/src/app/app.component.ts
+++ realpage-app/src/app/app.component.ts
@@ -5,7 +5,8 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   developer = 'Johnathan M. Tebeau';
   profile = 'https://media.licdn.com/dms/image/C5603AQHV94BM9VmKgw/profile-displayphoto-shrink_100_100/0?e=1545264000&v=beta&t=39aEWbAZ9VXtDeSnVeusru4aIOb5hL2KEoScYAPK6cs';
-};
+}
